a256e9a2289e2e74185ef62a9d26b067f105fc39
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-09-30 18:43:02 +0200

    Create GpioDetector.h

---
+++ src/SmartHouse/Logging/Stm32/GpioDetector.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef _MSC_VER
+	using HAL_GPIO_TypeDef = void*;
+	#define HAL_GPIO_ReadPin(port, pin) (0);
+#endif
+
+
+namespace SmartHouse::Logging::Stm32
+{
+	template<HAL_GPIO_TypeDef TGpio, uint16_t TPin, int TDetectionLevel = 0>
+	class GpioDetector
+	{
+	public:
+		bool IsDebuggerPresent() const
+		{
+			return HAL_GPIO_ReadPin(TGpio, TPin) == TDetectionLevel;
+		}
+	};
+}
