1ae3b032c815aa0acf79bf6fd42f5a793ac2d64b
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-09-30 18:53:10 +0200

    Update GpioDebuggerDetector.h

--- src/SmartHouse/Logging/Stm32/GpioDebuggerDetector.h
+++ src/SmartHouse/Logging/Stm32/GpioDebuggerDetector.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #ifdef _MSC_VER
-	using HAL_GPIO_TypeDef = void*;
+	using GPIO_TypeDef = void*;
 	#define HAL_GPIO_ReadPin(port, pin) (0);
 #endif
 
 
 namespace SmartHouse::Logging::Stm32
 {
-	template<HAL_GPIO_TypeDef TGpio, uint16_t TPin, int TDetectionLevel = 0>
+	template<GPIO_TypeDef TGpio, uint16_t TPin, int TDetectionLevel = 0>
 	class GpioDebuggerDetector
 	{
 	public:
